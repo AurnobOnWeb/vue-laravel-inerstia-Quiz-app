0eca071bfd7a6e00ebb801206afc7ec117565d06
Abir Hossen Ornob <abirhosain090@gmail.com>
2023-11-09 23:11:41 +0600

    fix Header

--- resources/js/shared/Header.vue
+++ resources/js/shared/Header.vue
@@ -1,20 +1,44 @@
 <script setup>
-import { Link } from "@inertiajs/vue3";
+import { Link, usePage } from "@inertiajs/vue3";
+import { computed } from "vue";
+const page = usePage();
+const appName = computed(() => page.props.appName);
 </script>
 <template>
     <div class="container">
         <header class="d-flex justify-content-center py-3">
+            <div class="col-md-3 mb-2 mb-md-0" style="margin-left: 50px">
+                <Link href="/" class="d-inline-flex link-body-emphasis">
+                    <span class="fs-4"> {{ appName }}</span>
+                </Link>
+            </div>
             <ul class="nav nav-pills">
                 <li class="nav-item">
-                    <Link href="/" class="nav-link active" aria-current="page"
+                    <Link
+                        href="/"
+                        :class="{ 'nav-link active': $page.url === '/' }"
+                        class="nav-link"
+                        aria-current="page"
                         >Home</Link
                     >
                 </li>
                 <li class="nav-item">
-                    <Link href="/quiz" class="nav-link">Quiz</Link>
+                    <Link
+                        href="/quiz"
+                        :class="{ 'nav-link active': $page.url === '/quiz' }"
+                        class="nav-link"
+                        >Quiz</Link
+                    >
                 </li>
                 <li class="nav-item">
-                    <Link href="/questions" class="nav-link">Question</Link>
+                    <Link
+                        href="/questions"
+                        :class="{
+                            'nav-link active': $page.url === '/questions',
+                        }"
+                        class="nav-link"
+                        >Question</Link
+                    >
                 </li>
             </ul>
         </header>
